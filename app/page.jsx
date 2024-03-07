6fa034fa460952a052cd6f7b6db24cac81ff0b6a
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-03-07 11:34:24 +0500

    h

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Footer from "@components/Footer";
 import HeroSection from "@components/HeroSection";
 import MainContent from "@components/MainContent";
-import Header from "@components/header";
+import Header from "@components/Header";
 
 
 export default function Home() {
